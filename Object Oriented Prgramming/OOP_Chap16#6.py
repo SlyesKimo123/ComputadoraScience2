c2b436a195360989ef9cb5100985d54afabecb11
SlyesKimo123 <lukechase203@gmail.com>
2017-10-09 16:31:52 -0700

    CS2 updated content

--- Object Oriented Prgramming/OOP_Chap16#6.py
+++ Object Oriented Prgramming/OOP_Chap16#6.py
@@ -1,3 +1,4 @@
+import math
 class Point:
     """ Point class for representing and manipulating x,y coordinates. """
 
@@ -15,7 +16,38 @@ class Point:
     def distanceFromOrigin(self):
         return ((self.x ** 2) + (self.y ** 2)) ** 0.5
 
+    def findEquation(self, punto):
+        x = self.getX()
+        xx = punto.getX()
+        y = self.getY()
+        yy = punto.getY()
+        slope = (x - xx) / (y - yy)
+        multiply = slope(x)
+        subtract = multiply - y
+        y_intercept = subtract
+        equation = print("y =", slope, "x +", y_intercept)
+        return equation
 
-p = Point(7, 6)
-print(p)
+    def move(self, dx, dy):
+        self.x = (self.x + dx)
+        self.y = (self.y + dy)
+        return self.x, self.y
 
+    def __str__(self):
+        return str(self.x) + ", " + str(self.y)
+
+    def circle(self, punto, punto_dos):
+        x = self.getX()
+        xx = punto.getX()
+        xxx = punto_dos.getX()
+        y = self.getY()
+        yy = punto.getY()
+        yyy = punto_dos.getY()
+        radius = (math.fabs(x) + math.fabs(xx))/2
+        print(radius)
+
+p = Point(2, 0)
+pp = Point(-2, 0)
+ppp = Point(0, 2)
+
+p.circle(pp, ppp)
